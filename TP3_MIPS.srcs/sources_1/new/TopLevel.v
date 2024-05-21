cfb6eb0af0e1da9ca3158f92319ba77be00ca84c
frankscitech <82125328+frankscitech@users.noreply.github.com>
2024-05-20 22:03:35 -0300

    [MODIF] TopLevel.v , agrego clockignore extras y movi clk_wiz

--- TP3_MIPS.srcs/sources_1/new/TopLevel.v
+++ TP3_MIPS.srcs/sources_1/new/TopLevel.v
@@ -23,34 +23,6 @@ module TopLevel
     output wire o_tx_fromPin
 );
 
-/*
-     Internals Wires:
-*/
-// Multiple Etapas
-wire                        w_clockIgnore_fromDUToEtapas;
-
-// For Etapa 1 (Program Counter)
-wire [INS_LEN-1:0]          w_pc_fromPcToDU;
-
-// For Etapa 1 (Instructions Memory)
-wire                        w_we_fromDUToInsMem;
-wire [INSMEM_ADDR_LEN-1:0]  w_addr_fromDUToInsMem;
-wire [INSMEM_DAT_LEN-1:0]   w_data_fromDUToInsMem;
-wire                        w_muxSel_fromDUToInsMemMux;
-
-// For Etapa 2 (Register File)
-wire [REGFILE_ADDR_LEN-1:0]  w_addr_fromDUToRegFile; // Address to read from Register File
-wire [REGFILE_LEN-1:0]       w_data_fromRegFileToDU; // Data read from Register File
-wire                         w_muxSel_fromDUToRegFileMux; //  Mux Selector
-
-// For Etapa 2 (From Control Unit  to Debug Unit)
-wire                        w_halt_fromCUToDU;
-
-// For Etapa 4 (Data Memory)
-wire [DATMEM_ADDR_LEN-1:0]    w_addr_fromDUToDatMem; // Address to read from Data Memory
-wire [DAT_LEN-1:0]            w_data_fromDatMemToDU; // Data read from Data memory
-wire                          w_muxSel_fromDUToDatMemMux; //  Mux Selector
-wire                          w_re_fromDUToDatMem; // Read Enable
 
 wire clk_out;
 wire o_locked;
@@ -63,7 +35,40 @@ clk_wiz_0 u_ClockWizard
     .locked(o_locked),       // output locked
    // Clock in ports
     .clk_in1(i_clock_fromPin)
-); 
+);
+
+/*
+     Internals Wires:
+*/
+// For Multiple Etapas
+wire    w_clockIgnore_fromDUToPcAndLatches;
+
+// For Etapa 1 (Program Counter)
+wire [INS_LEN-1:0]          w_pc_fromPcToDU;
+
+// For Etapa 1 (Instructions Memory)
+wire                        w_we_fromDUToInsMem;
+wire [INSMEM_ADDR_LEN-1:0]  w_addr_fromDUToInsMem;
+wire [INSMEM_DAT_LEN-1:0]   w_data_fromDUToInsMem;
+wire                        w_muxSel_fromDUToInsMemMux;
+wire                         w_clockIgnore_fromDUToInsMem;
+
+
+// For Etapa 2 (Register File)
+wire [REGFILE_ADDR_LEN-1:0]  w_addr_fromDUToRegFile; // Address to read from Register File
+wire [REGFILE_LEN-1:0]       w_data_fromRegFileToDU; // Data read from Register File
+wire                         w_muxSel_fromDUToRegFileMux; //  Mux Selector
+wire                         w_clockIgnore_fromDUToRegFile;
+
+// For Etapa 2 (From Control Unit  to Debug Unit)
+wire                        w_halt_fromCUToDU;
+
+// For Etapa 4 (Data Memory)
+wire [DATMEM_ADDR_LEN-1:0]    w_addr_fromDUToDatMem; // Address to read from Data Memory
+wire [DAT_LEN-1:0]            w_data_fromDatMemToDU; // Data read from Data memory
+wire                          w_muxSel_fromDUToDatMemMux; //  Mux Selector
+wire                          w_re_fromDUToDatMem; // Read Enable
+wire                          w_clockIgnore_fromDUToDatMem;
 
 DebugUnit
 #(
@@ -74,25 +79,27 @@ DebugUnit
     // Multiple Etapas
     .i_globalClock          (clk_out),
     .i_globalReset          (i_reset_fromPin|!o_locked),
-    .o_clockIgnore_fromDUToEtapas(w_clockIgnore_fromDUToEtapas),
+    .o_clockIgnore_fromDUToPcAndLatches (w_clockIgnore_fromDUToPcAndLatches),   
 
     // For Uart
     .i_rx_fromTopToDU       (i_rx_fromPin),
     .o_tx_fromDUToTop       (o_tx_fromPin),
 
     // For Etapa 1 (Program Counter)
-    .i_pc_fromPcToDU        (w_pc_fromPcToDU), 
+    .i_pc_fromPcToDU        (w_pc_fromPcToDU),  
 
     // For Etapa 1 (Instructions Memory)
     .o_we_fromDUToInsMem        (w_we_fromDUToInsMem),
     .o_muxSel_fromDUToInsMemMux (w_muxSel_fromDUToInsMemMux),
     .o_addr_fromDUToInsMem      (w_addr_fromDUToInsMem),
     .o_data_fromDUToInsMem      (w_data_fromDUToInsMem), 
+    .o_clockIgnore_fromDUToInsMem(w_clockIgnore_fromDUToInsMem),    
 
     // For Etapa 2 (Register File)
-    .o_addr_fromDUToRegFile     (w_addr_fromDUToRegFile), // Address to read from Register File
-    .i_data_fromRegFileToDU     (w_data_fromRegFileToDU), // Data read from Register File
-    .o_muxSel_fromDUToRegFileMux(w_muxSel_fromDUToRegFileMux), // Mux Selector 
+    .o_addr_fromDUToRegFile         (w_addr_fromDUToRegFile), // Address to read from Register File
+    .i_data_fromRegFileToDU         (w_data_fromRegFileToDU), // Data read from Register File
+    .o_muxSel_fromDUToRegFileMux    (w_muxSel_fromDUToRegFileMux), // Mux Selector 
+    .o_clockIgnore_fromDUToRegFile  (w_clockIgnore_fromDUToRegFile),    
 
     // For Etapa 2 (From Control Unit  to Debug Unit)
     .i_halt_fromCUToDU      (w_halt_fromCUToDU),
@@ -101,7 +108,8 @@ DebugUnit
     .o_addr_fromDUToDatMem          (w_addr_fromDUToDatMem), // Address to read from Data Memory
     .i_data_fromDatMemToDU          (w_data_fromDatMemToDU), // Data read from Data memory
     .o_muxSel_fromDUToDatMemMux     (w_muxSel_fromDUToDatMemMux), // Mux Selector
-    .o_re_fromDUToDatMem            (w_re_fromDUToDatMem)
+    .o_re_fromDUToDatMem            (w_re_fromDUToDatMem),
+    .o_clockIgnore_fromDUToDatMem   (w_clockIgnore_fromDUToDatMem)    
 );
 
 Etapas
@@ -113,21 +121,23 @@ u_Etapas
     // Multiple Etapas
     .i_globalClock          (clk_out),
     .i_globalReset          (i_reset_fromPin|!o_locked),
-    .i_clockIgnore_fromDU   (w_clockIgnore_fromDUToEtapas),
+    .i_clockIgnore_fromDUToPcAndLatches (w_clockIgnore_fromDUToPcAndLatches),
 
     // For Etapa 1 (Program Counter)
-    .o_pc_fromE1ToDU        (w_pc_fromPcToDU), 
+    .o_pc_fromE1ToDU        (w_pc_fromPcToDU),     
 
     // For Etapa 1 (Instructions Memory)
     .i_weForInsMem_fromDUToE1        (w_we_fromDUToInsMem),
     .i_addrForInsMem_fromDUToE1      (w_addr_fromDUToInsMem),
     .i_dataForInsMem_fromDUToE1      (w_data_fromDUToInsMem), 
     .i_muxSelForInsMemMux_fromDUToE1 (w_muxSel_fromDUToInsMemMux), // Mux Selector
+    .i_clockIgnore_fromDUToInsMem    (w_clockIgnore_fromDUToInsMem),    
 
     // For Etapa 2 (Register File)
     .i_addrForRegFile_fromDUToE2     (w_addr_fromDUToRegFile), // Address to read from Register File
     .o_dataForRegFile_fromE2ToDU     (w_data_fromRegFileToDU), // Data read from Register File
     .i_muxSelForRegFileMux_fromDUToE2(w_muxSel_fromDUToRegFileMux), // Mux Selector
+    .i_clockIgnore_fromDUToRegFile   (w_clockIgnore_fromDUToRegFile),
 
     // For Etapa 2 (From Control Unit  to Debug Unit)
     .o_halt_fromE4ToDU      (w_halt_fromCUToDU),
@@ -136,8 +146,8 @@ u_Etapas
     .i_addrForDatMem_fromDUToE4      (w_addr_fromDUToDatMem), // Address to read from Data Memory
     .o_dataForDatMem_fromE4ToDU      (w_data_fromDatMemToDU), // Data read from Data memory
     .i_muxSelForDatMemMux_fromDUToE4 (w_muxSel_fromDUToDatMemMux), // Mux Selector
-    .i_reForDatMem_fromDUToE4        (w_re_fromDUToDatMem)
-
+    .i_reForDatMem_fromDUToE4        (w_re_fromDUToDatMem),
+    .i_clockIgnore_fromDUToDatMem    (w_clockIgnore_fromDUToDatMem)
 );
 
 
