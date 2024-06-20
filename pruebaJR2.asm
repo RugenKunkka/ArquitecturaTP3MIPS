68504629fb689a8dacfe93dd822d1a8cdf33023e
Stefano <stefano.itr.unc@gmail.com>
2024-06-19 21:28:20 -0300

    programa JR2 para que pruebe franco

---
+++ pruebaJR2.asm
@@ -0,0 +1,24 @@
+	ori	r1 , r18 , 52 // 13*4
+	ori	r2 , r18 , 80 // 20*4
+	ori	r3 , r18 , 3
+	ori	r4 , r18 , 4
+	ori	r5 , r18 , 5
+	jr r1 //salta
+	ori r6, r18, 6
+	ori r7, r18, 7
+	ori r8, r18, 8
+	ori r9, r18, 9
+	ori r10, r18, 10
+	ori r11, r18, 11
+	ori r12, r18, 11
+	ori r13, r18, 13 //aca tiene q saltar
+	ori r14 , r18, 14
+	ori r15 , r18, 15
+	ori r16 , r18, 16
+	ori r17 , r18, 17
+	ori r19 , r18, 19
+	ori r20 , r18, 20 //aca tiene q saltar
+	ori r21 , r18, 21
+	halt
+	nop
+	nop
